7c20efa3e4e2b7850543f2a9bbd4412e51efa22d
BEATRIZ PIFFER SILVA <81167411+Beatrizpiffer@users.noreply.github.com>
2022-03-24 09:51:25 -0300

    Add files via upload

---
+++ 01- Fundamentos_básicos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		
+		<title>Fundamentos básicos</title>
+	</head>
+
+	<body>
+
+	     <?php
+//esse é meu primeiro arquivo php
+	     echo "My name is Beatriz <br>";
+	     print "Minha idade é 23";
+	     ?>
+
+	</body>
+	</html>
+
