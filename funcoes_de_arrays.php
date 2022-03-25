182be711c3c8b4243f5994efcd35ed492dd8b0a5
BEATRIZ PIFFER SILVA <81167411+Beatrizpiffer@users.noreply.github.com>
2022-03-25 09:51:23 -0300

    Add files via upload

---
+++ funcoes_de_arrays.php
@@ -0,0 +1,26 @@
+<?php
+	/*
+	*$carros = array("Camaro", "Uno", "Gol");
+	*print_r($carros);
+	*echo "<br>";
+	*echo array_shift($carros);
+	*echo "<br>";
+	*print_r($carros);
+
+
+$soma = array(5,6,10,8);
+$total = array_sum($soma);
+echo array_sum($soma);
+*/
+$nomes = array("Rodrigo", "Carlos", "Neusa", "Talita");
+
+$string = implode(", ", $nomes);
+
+echo $string;
+
+
+
+
+
+
+
