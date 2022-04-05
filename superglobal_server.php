d0c69b9da427ac67bf37b51d4a9e0106f424578a
BEATRIZ PIFFER SILVA <81167411+Beatrizpiffer@users.noreply.github.com>
2022-04-05 11:15:28 -0300

    Create superglobal_server.php

---
+++ superglobal_server.php
@@ -0,0 +1,21 @@
+<?php
+//Superglobais 
+/*
+  $GLOBALS
+  $_SERVER
+  $_REQUEST
+  $_POST
+  $_GET
+  $_FILES
+  $_ENV
+  $_COOKIE
+  $_SESSION
+*/
+
+echo $_SERVER['PHP_SELF']."<br>";
+echo $_SERVER['SEVER_NAME']."<br>";
+echo $_SERVER['SCRIPT_FILENAME']."<br>";
+echo $_SERVER['DOCUMENT_ROOT']."<br>";
+echo $_SERVER['SERVER_PORT']."<br>";
+echo $_SERVER['SERVER_ADDR']."<br>";
+
