4d63232d7758faecb01ba8e487ad41a08a302fb2
BEATRIZ PIFFER SILVA <81167411+Beatrizpiffer@users.noreply.github.com>
2022-04-06 11:43:30 -0300

    Create Primeira_parte_dados.php

---
+++ Primeira_parte_dados.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+	
+	<form action="dados.php" method="POST">
+		Nome: <input type="tex" name="nome"><br>
+		Email:<input type="email" name="email"><br>
+		<button type="submit"> Enviar </button>
+	</form>
+
+</body>
+</html>
+
+
+
+
